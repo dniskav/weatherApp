a04a91b4c5c71d5e2e226f0c392eb58a269f9464
Dniskav <dniskav@gmail.com>
2025-04-02 13:31:34 +0200

    fix: app config clean

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
 import { provideRouter } from '@angular/router'
-import { provideAnimations } from '@angular/platform-browser/animations'
 import { provideHttpClient } from '@angular/common/http'
-import { providePrimeNG } from 'primeng/config'
-import Lara from '@primeng/themes/lara'
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimations(),
     provideHttpClient(),
-    providePrimeNG({
-      theme: {
-        preset: Lara
-      },
-      ripple: false
-    })
+    provideAnimationsAsync()
   ]
 }
